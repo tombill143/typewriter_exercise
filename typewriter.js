0de9e8948c051ba133361886e65705960e0d9da3
Tom Billington <tombill143@gmail.com>
2023-02-07 17:19:00 +0100

    finished the typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,56 +1,38 @@
 "use strict";
-let word = "My Name is Tom and I am amazing";
+let word = "This text is written one character at a time.";
 let letters = word;
-/*let letters = [
-  "M",
-  "y",
-  " ",
-  "n",
-  "a",
-  "m",
-  "e",
-  " ",
-  "i",
-  "s",
-  " ",
-  "T",
-  "o",
-  "m",
-  " ",
-  "a",
-  "n",
-  "d",
-  " ",
-  "i",
-  " ",
-  "a",
-  "m",
-  " ",
-  "a",
-  "m",
-  "a",
-  "z",
-  "i",
-  "n",
-  "g",
-];*/
 
 let i = 0;
 
 function print() {
   if (i < letters.length) {
     console.log(letters[i]);
-
     document.querySelector(
       "#typewriter"
     ).innerHTML += `<span>${letters[i]}</span>`;
+    const audio = document.getElementById("typekey1");
+    const audio2 = document.getElementById("typekey2");
+    const audio3 = document.getElementById("typespace");
+    const audio4 = document.getElementById("typelast");
+    const audio5 = document.getElementById("typereturn");
+
+    audio.play();
+    audio2.play();
+    if (letters[i] === " ") {
+      const audio = document.getElementById("typespace");
+      audio3.play();
+    }
+
+    if (letters[i] === ".") {
+      const audio = document.getElementById("typespace");
+      audio4.play();
+      audio5.play();
+    }
+
     i++;
+
     setTimeout(print, Math.random() * 1000); // delay of 1 second
   }
 }
 
 print();
-
-/*for (let i = 0; i < letters.length; i++) {
-  console.log(letters[i]);
-}*/
